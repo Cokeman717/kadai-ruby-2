3880e70d20c582d79284eee8cec00f603b8393fd
cokeman <cokeredder@gmail.com>
2021-05-31 11:02:35 +0000

    kadai-ruby-2

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,17 @@
 def fizzbuzz(num)
-    (1..num).each do |number|
-        if number % 15 == 0
-            puts 'FizzBuZZ'
-        elsif number % 3 == 0
-            puts 'Fizz'
-        elsif number % 5 == 0
-            puts 'Buzz'
-        else 
-            puts number
-        end
+    if num % 15 == 0
+        'FizzBuZZ'
+    elsif num % 3 == 0
+        'Fizz'
+    elsif num % 5 == 0
+        'Buzz'
+    else 
+        num
     end
 end
 
 num_max = 100
 
-fizzbuzz num_max
+(1..num_max).each do |number|
+    puts fizzbuzz(number)
+end
